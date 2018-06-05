d66d1808c6b2f92ac767a7d1a2e78f047d401449
Brian Anderson <andersrb@gmail.com>
2018-06-04 18:31:04 -0700

    Test CommonMark HTML rendering conformance

--- script/test.sh
+++ script/test.sh
@@ -40,6 +40,9 @@ run python3 script/spec_tests.py --rtjson --spec specs/rtjson/rtjson.spec
 run python3 script/spec_tests.py --rtjson --spec specs/rtjson/bugs.spec
 run python2 script/stack_smash_test.py
 
+# Also test commonmark HTML rendering
+run python3 script/spec_tests.py --spec specs/html/spec-gfm.txt -p "target/$SM_TARGET/snoomark"
+
 if [[ errors -ne 0 ]]; then
 	echo -e "\nsome tests failed\n"
 	exit 1
